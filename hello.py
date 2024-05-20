2526ea381fe840618bc2ebc75bbb7b1b005f919a
Benjamin Sparwasser <benjaminsparwasser@gmail.com>
2024-05-20 13:55:26 +0200

    spacy first steps

---
+++ hello.py
@@ -0,0 +1,7 @@
+import spacy
+
+# nlp = spacy.load('en_core_web_sm')
+nlp = spacy.load('ru_core_news_sm')
+doc = nlp(u'севодня меня кто-то в метро лизнул жею')
+for token in doc:
+    print(token.text, token.lemma_)
